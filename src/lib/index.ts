cec38c6c67b414749c2c11a84760b990ce884b1b
yunho <kimyouknow@naver.com>
2022-06-19 21:18:35 +0900

    Rename: hooks폴더 Hooks폴더로 변경

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
-import usePopup from './hooks/usePopup';
-import useToggle from './hooks/useToggle';
+import usePopup from './Hooks/usePopup';
+import useToggle from './Hooks/useToggle';
 import Modal from './Modal';
 
 export { Modal as CustomModal, useToggle, usePopup };
