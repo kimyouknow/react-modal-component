c26015110f3f1d409027e0f8bca8ffed11044b4e
yunho <kimyouknow@naver.com>
2022-06-19 17:12:21 +0900

    Refactor: App컴포넌트삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from '@/App';
+import Example from '@/Example';
 
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
 
 root.render(
   <StrictMode>
-    <App />
+    <Example />
   </StrictMode>,
 );
